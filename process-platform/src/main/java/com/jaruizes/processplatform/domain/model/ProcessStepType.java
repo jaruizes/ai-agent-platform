ef2f58cbeb178d14bac840e8ee14614319a9fcd8
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-23 16:44:38 +0200

    M9.2 add ProcessStepType.java

---
+++ process-platform/src/main/java/com/jaruizes/processplatform/domain/model/ProcessStepType.java
@@ -0,0 +1,12 @@
+package com.jaruizes.processplatform.domain.model;
+
+public enum ProcessStepType {
+    SERVICE,
+    TOOL,
+    AGENT,
+    AGENTIC_EXECUTION,
+    DECISION,
+    HUMAN,
+    WAIT_EVENT,
+    SUBPROCESS
+}
