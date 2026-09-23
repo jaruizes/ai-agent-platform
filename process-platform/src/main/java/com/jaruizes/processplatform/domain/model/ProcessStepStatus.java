18cc3282d2640619d98f8eaca06be01bff083c5d
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-23 16:44:43 +0200

    M9.2 add ProcessStepStatus.java

---
+++ process-platform/src/main/java/com/jaruizes/processplatform/domain/model/ProcessStepStatus.java
@@ -0,0 +1,12 @@
+package com.jaruizes.processplatform.domain.model;
+
+public enum ProcessStepStatus {
+    PENDING,
+    READY,
+    RUNNING,
+    WAITING,
+    COMPLETED,
+    FAILED,
+    CANCELLED,
+    SKIPPED
+}
