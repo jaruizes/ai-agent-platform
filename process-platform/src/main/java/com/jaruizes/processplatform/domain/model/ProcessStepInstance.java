9f937b01c97cfd908f2a58c02413a32c731e4b9f
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-23 16:44:50 +0200

    M9.2 add ProcessStepInstance.java

---
+++ process-platform/src/main/java/com/jaruizes/processplatform/domain/model/ProcessStepInstance.java
@@ -0,0 +1,20 @@
+package com.jaruizes.processplatform.domain.model;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+
+public record ProcessStepInstance(
+        UUID id,
+        UUID processInstanceId,
+        UUID stepDefinitionId,
+        String stepKey,
+        ProcessStepType type,
+        ProcessStepStatus status,
+        Map<String,Object> input,
+        Map<String,Object> output,
+        Map<String,Object> error,
+        UUID delegatedExecutionId,
+        Instant startedAt,
+        Instant completedAt,
+        Instant updatedAt) {}
