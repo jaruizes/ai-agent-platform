cbb5df62bb91967f7c8f3d68197b09f56e456e86
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-24 11:58:51 +0200

    M9.4 expose process service adapter configuration

--- process-platform/src/main/java/com/jaruizes/processplatform/infrastructure/api/rest/dto/UpdateProcessServiceRequest.java
+++ process-platform/src/main/java/com/jaruizes/processplatform/infrastructure/api/rest/dto/UpdateProcessServiceRequest.java
@@ -7,5 +7,6 @@ public record UpdateProcessServiceRequest(
         @NotBlank String name,
         String description,
         @NotBlank String implementationKey,
+        Map<String,Object> configuration,
         Map<String,Object> inputSchema,
         Map<String,Object> outputSchema) {}
