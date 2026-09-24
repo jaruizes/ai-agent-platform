41f9140e1bc02fbbcc37c3e29b24078ceccff658
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-24 11:05:53 +0200

    M9.4 add CreateProcessServiceRequest.java

---
+++ process-platform/src/main/java/com/jaruizes/processplatform/infrastructure/api/rest/dto/CreateProcessServiceRequest.java
@@ -0,0 +1,15 @@
+package com.jaruizes.processplatform.infrastructure.api.rest.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Map;
+
+public record CreateProcessServiceRequest(
+        @NotBlank String serviceKey,
+        @NotBlank String name,
+        String description,
+        @Min(1) int version,
+        @NotBlank String implementationKey,
+        Map<String,Object> inputSchema,
+        Map<String,Object> outputSchema) {}
