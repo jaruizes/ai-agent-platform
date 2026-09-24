71fb3e86176bde82c170a33ace6ea8efbdd1a078
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-24 11:17:16 +0200

    M9.4 add UpdateProcessServiceRequest.java

---
+++ process-platform/src/main/java/com/jaruizes/processplatform/infrastructure/api/rest/dto/UpdateProcessServiceRequest.java
@@ -0,0 +1,11 @@
+package com.jaruizes.processplatform.infrastructure.api.rest.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import java.util.Map;
+
+public record UpdateProcessServiceRequest(
+        @NotBlank String name,
+        String description,
+        @NotBlank String implementationKey,
+        Map<String,Object> inputSchema,
+        Map<String,Object> outputSchema) {}
